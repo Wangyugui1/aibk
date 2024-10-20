b5653e3b0c068ff91e5d9d3167fd524008e458ed
Wang Yugui <150242504+Wangyugui1@users.noreply.github.com>
2024-10-20 23:01:10 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,30 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from config import Config
+from flask_sqlalchemy import SQLAlchemy    # SQLAlchemy: 用于数据库操作
+from flask_login import LoginManager    # LoginManager: 用于处理用户登录
+from config import Config    # Config: 包含应用配置的类
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+db = SQLAlchemy()    # 创建SQLAlchemy实例,用于数据库操作
+login_manager = LoginManager()     # 创建LoginManager实例,用于管理用户登录
 
+# 一个应用工厂函数,用于创建和配置Flask应用实例
 def create_app():
+    
+    #  创建Flask应用实例
     app = Flask(__name__)
+
+    # 从Config对象加载配置
     app.config.from_object(Config)
 
+    # 初始化数据库与应用的连接
     db.init_app(app)
+
+    # 初始化登录管理器
     login_manager.init_app(app)
+
+    # 设置登录页面的视图函数
     login_manager.login_view = 'auth.login'
 
+    # 导入了两个蓝图(main和auth),并将它们注册到应用中
     from app.routes import main, auth
     app.register_blueprint(main)
     app.register_blueprint(auth)
